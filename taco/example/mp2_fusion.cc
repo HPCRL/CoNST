a53d9bb7fb65631ac465185fe2c1c0586d1d4dd8
Saurabh <saurabh.raje@utah.edu>
2024-03-06 19:48:45 -0700

    pick up generated header

--- taco/example/mp2_fusion.cc
+++ taco/example/mp2_fusion.cc
@@ -7,54 +7,63 @@
 #include <sys/stat.h>
 #include <taco/storage/file_io_tns.h>
 #include <vector>
+#include "filter_fused.hpp"
+#include "nofilter_fused.hpp"
 
 using namespace taco;
 
-const int MO = 100;
-const int PAO = 300;
-const int PAO_HAT = 300;
-const int AUX = 700;
+// const int MO = 100;
+// const int PAO = 300;
+// const int PAO_HAT = 300;
+// const int AUX = 700;
 
-void noncov_mp2_fused(Tensor<double> Int, Tensor<double> C, Tensor<double> Phat,
-                      Tensor<double> X, Tensor<double> L) {
-  IndexVar nu("nu");
-  IndexVar muhat("muhat");
-  IndexVar k("k");
-  IndexVar mu("mu");
-  IndexVar i("i");
-  TensorVar Int_var = Int.getTensorVar();
-  std::cout<<"Shape of int is "<<Int_var.getType().getShape()<<std::endl;
-  TensorVar C_var = C.getTensorVar();
-  TensorVar Phat_var = Phat.getTensorVar();
-  TensorVar X_var = X.getTensorVar();
-  TensorVar L_var = L.getTensorVar();
-  std::cout<<"Shape of L is "<<L_var.getType().getShape()<<std::endl;
-  TensorVar IntC =
-      TensorVar("IntC", Type(Int.getComponentType(), {PAO}), {taco::dense});
-  TensorVar IntCPhat =
-      TensorVar("IntCPhat", Type(Int.getComponentType(), {PAO_HAT}), {taco::dense});
-  auto fused_ir = forall(
-      k,
-      forall(
-          i,
-          where(forall(muhat,
-                       X_var(k, i, muhat) += IntCPhat(muhat) * L_var(k, i)),
-                where(forall(mu, forall(muhat, IntCPhat(muhat) +=
-                                               IntC(mu) * Phat_var(mu, muhat))),
-                      forall(nu, forall(mu, IntC(mu) += Int_var(k, nu, mu) *
-                                                        C_var(i, nu)))))));
-  ;
-  X(k, i, muhat) = Int(k, nu, mu) * C(i, nu) * Phat(mu, muhat) * L(k, i);
-  X.compile(fused_ir);
-  auto start = std::chrono::high_resolution_clock::now();
-  X.assemble();
-  X.compute();
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> elapsed = end - start;
-  std::cout << "Time noncov_mp2_fused:  " << elapsed.count() << " ms "
-            << std::endl;
-  //write("fused_3cint.tns", X);
-}
+const int MO = 25;
+const int PAO = 75;
+const int PAO_HAT = 75;
+const int AUX = 175;
+// void noncov_mp2_fused(Tensor<double> Int, Tensor<double> C, Tensor<double>
+// Phat,
+//                       Tensor<double> X, Tensor<double> L) {
+//   IndexVar nu("nu");
+//   IndexVar muhat("muhat");
+//   IndexVar k("k");
+//   IndexVar mu("mu");
+//   IndexVar i("i");
+//   TensorVar Int_var = Int.getTensorVar();
+//   std::cout<<"Shape of int is "<<Int_var.getType().getShape()<<std::endl;
+//   TensorVar C_var = C.getTensorVar();
+//   TensorVar Phat_var = Phat.getTensorVar();
+//   TensorVar X_var = X.getTensorVar();
+//   TensorVar L_var = L.getTensorVar();
+//   std::cout<<"Shape of L is "<<L_var.getType().getShape()<<std::endl;
+//   TensorVar IntC =
+//       TensorVar("IntC", Type(Int.getComponentType(), {PAO}), {taco::dense});
+//   TensorVar IntCPhat =
+//       TensorVar("IntCPhat", Type(Int.getComponentType(), {PAO_HAT}),
+//       {taco::dense});
+//   auto fused_ir = forall(
+//       k,
+//       forall(
+//           i,
+//           where(forall(muhat,
+//                        X_var(k, i, muhat) += IntCPhat(muhat) * L_var(k, i)),
+//                 where(forall(mu, forall(muhat, IntCPhat(muhat) +=
+//                                                IntC(mu) * Phat_var(mu,
+//                                                muhat))),
+//                       forall(nu, forall(mu, IntC(mu) += Int_var(k, nu, mu) *
+//                                                         C_var(i, nu)))))));
+//   ;
+//   X(k, i, muhat) = Int(k, nu, mu) * C(i, nu) * Phat(mu, muhat) * L(k, i);
+//   X.compile(fused_ir);
+//   auto start = std::chrono::high_resolution_clock::now();
+//   X.assemble();
+//   X.compute();
+//   auto end = std::chrono::high_resolution_clock::now();
+//   std::chrono::duration<double, std::milli> elapsed = end - start;
+//   std::cout << "Time noncov_mp2_fused:  " << elapsed.count() << " ms "
+//             << std::endl;
+//   //write("fused_3cint.tns", X);
+// }
 
 void noncov_mp2_unfused(Tensor<double> Int, Tensor<double> C,
                         Tensor<double> Phat, Tensor<double> X,
@@ -107,66 +116,67 @@ void noncov_mp2_nary(Tensor<double> Int, Tensor<double> C, Tensor<double> Phat,
   std::chrono::duration<double, std::milli> elapsed = end - start;
   std::cout << "Time noncov_mp2_nary:  " << elapsed.count() << " ms "
             << std::endl;
-  //write("nary_3cint.tns", X);
+  // write("nary_3cint.tns", X);
 }
 
-void nofilter_fused(Tensor<double> Int, Tensor<double> C, Tensor<double> X,
-                    Tensor<double> Phat) {
-  IndexVar i("i");
-  IndexVar mu("mu");
-  IndexVar muhat("muhat");
-  IndexVar nu("nu");
-  IndexVar k("k");
-  TensorVar Int_var = Int.getTensorVar();
-  TensorVar C_var = C.getTensorVar();
-  TensorVar X_var = X.getTensorVar();
-  Phat = Phat.transpose({1, 0});
-  Phat.setName("Phat");
-  TensorVar Phat_var = Phat.getTensorVar();
-  TensorVar IntC =
-      TensorVar("IntC", Type(Int.getComponentType(), {PAO}), {taco::dense});
-  auto fused_ir = forall(
-      k,
-      forall(i, where(forall(muhat, forall(mu, X_var(k, i, muhat) +=
-                                               IntC(mu) * Phat_var(muhat, mu))),
-                      forall(nu, forall(mu, IntC(mu) += Int_var(k, nu, mu) *
-                                                        C_var(i, nu))))));
-  X(k, i, muhat) = Int(k, nu, mu) * C(i, nu) * Phat(muhat, mu);
-  X.compile(fused_ir);
-  auto start = std::chrono::high_resolution_clock::now();
-  X.assemble();
-  X.compute();
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> elapsed = end - start;
-  std::cout << "Time nofilter_fused:  " << elapsed.count() << " ms "
-            << std::endl;
-  //write("data_3cent_real/threec_int.tns", X);
-}
+// void nofilter_fused(Tensor<double> Int, Tensor<double> C, Tensor<double> X,
+//                     Tensor<double> Phat) {
+//   IndexVar i("i");
+//   IndexVar mu("mu");
+//   IndexVar muhat("muhat");
+//   IndexVar nu("nu");
+//   IndexVar k("k");
+//   TensorVar Int_var = Int.getTensorVar();
+//   TensorVar C_var = C.getTensorVar();
+//   TensorVar X_var = X.getTensorVar();
+//   Phat = Phat.transpose({1, 0});
+//   Phat.setName("Phat");
+//   TensorVar Phat_var = Phat.getTensorVar();
+//   TensorVar IntC =
+//       TensorVar("IntC", Type(Int.getComponentType(), {PAO}), {taco::dense});
+//   auto fused_ir = forall(
+//       k,
+//       forall(i, where(forall(muhat, forall(mu, X_var(k, i, muhat) +=
+//                                                IntC(mu) * Phat_var(muhat,
+//                                                mu))),
+//                       forall(nu, forall(mu, IntC(mu) += Int_var(k, nu, mu) *
+//                                                         C_var(i, nu))))));
+//   X(k, i, muhat) = Int(k, nu, mu) * C(i, nu) * Phat(muhat, mu);
+//   X.compile(fused_ir);
+//   auto start = std::chrono::high_resolution_clock::now();
+//   X.assemble();
+//   X.compute();
+//   auto end = std::chrono::high_resolution_clock::now();
+//   std::chrono::duration<double, std::milli> elapsed = end - start;
+//   std::cout << "Time nofilter_fused:  " << elapsed.count() << " ms "
+//             << std::endl;
+//   //write("data_3cent_real/threec_int.tns", X);
+// }
 
-void nofilter_unfused(Tensor<double> Int, Tensor<double> C, Tensor<double> X,
-                      Tensor<double> Phat) {
-  IndexVar i("i");
-  IndexVar mu("mu");
-  IndexVar muhat("muhat");
-  IndexVar nu("nu");
-  IndexVar k("k");
-  Tensor<double> IntC =
-      Tensor<double>("IntC", {AUX, MO, PAO}, {Dense, Sparse, Sparse});
-  IntC(k, i, mu) = Int(k, nu, mu) * C(i, nu);
-  X(k, i, muhat) = IntC(k, i, mu) * Phat(mu, muhat);
-  IntC.compile();
-  X.compile();
-  auto start = std::chrono::high_resolution_clock::now();
-  IntC.assemble();
-  IntC.compute();
-  X.assemble();
-  X.compute();
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> elapsed = end - start;
-  std::cout << "Time nofilter_unfused:  " << elapsed.count() << " ms "
-            << std::endl;
-  // write("data_3cent/X_nofilter_unf.tns", X);
-}
+// void nofilter_unfused(Tensor<double> Int, Tensor<double> C, Tensor<double> X,
+//                       Tensor<double> Phat) {
+//   IndexVar i("i");
+//   IndexVar mu("mu");
+//   IndexVar muhat("muhat");
+//   IndexVar nu("nu");
+//   IndexVar k("k");
+//   Tensor<double> IntC =
+//       Tensor<double>("IntC", {AUX, MO, PAO}, {Dense, Sparse, Sparse});
+//   IntC(k, i, mu) = Int(k, nu, mu) * C(i, nu);
+//   X(k, i, muhat) = IntC(k, i, mu) * Phat(mu, muhat);
+//   IntC.compile();
+//   X.compile();
+//   auto start = std::chrono::high_resolution_clock::now();
+//   IntC.assemble();
+//   IntC.compute();
+//   X.assemble();
+//   X.compute();
+//   auto end = std::chrono::high_resolution_clock::now();
+//   std::chrono::duration<double, std::milli> elapsed = end - start;
+//   std::cout << "Time nofilter_unfused:  " << elapsed.count() << " ms "
+//             << std::endl;
+//   // write("data_3cent/X_nofilter_unf.tns", X);
+// }
 
 void nofilter_nary(Tensor<double> Int, Tensor<double> C, Tensor<double> X,
                    Tensor<double> Phat) {
@@ -191,41 +201,41 @@ void nofilter_nary(Tensor<double> Int, Tensor<double> C, Tensor<double> X,
 }
 
 int main() {
-  //Tensor<double> Int("Int", {PAO, PAO, AUX}, {Dense, Sparse, Sparse});
-  Tensor<double> Int = read("data_3cent_realmid/Int.tns", {Dense, Sparse, Sparse});
+  // Tensor<double> Int("Int", {PAO, PAO, AUX}, {Dense, Sparse, Sparse});
+  Tensor<double> Int = read("data_3cent_real/Int.tns", {Dense, Sparse, Sparse});
   Int.pack();
-  Int = Int.transpose({2, 0, 1});
+  // Int = Int.transpose({0, 2, 1});
   Int.setName("Int");
 
-  //Tensor<double> L("L", {AUX, MO}, {Dense, Sparse});
-  Tensor<double> L = read("data_3cent_realmid/L.tns", {Dense, Sparse});
+  // Tensor<double> L("L", {AUX, MO}, {Dense, Sparse});
+  Tensor<double> L = read("data_3cent_real/L.tns", {Dense, Sparse});
   L.pack();
   L.setName("L");
 
   Tensor<double> Phat("Phat", {PAO, PAO_HAT}, {Dense, Sparse});
-  Phat = read("data_3cent_realmid/Phat.tns", {Dense, Sparse});
+  Phat = read("data_3cent_real/Phat.tns", {Dense, Sparse});
   Phat.pack();
   Phat.setName("Phat");
 
   Tensor<double> C("C", {PAO, MO}, {Dense, Sparse});
-  C = read("data_3cent_realmid/C.tns", {Dense, Sparse});
+  C = read("data_3cent_real/C.tns", {Dense, Sparse});
   Phat.pack();
-  C = C.transpose({1, 0});
+  // C = C.transpose({1, 0});
   C.setName("C");
 
-  //Tensor<double> X("X", {AUX, MO, PAO_HAT}, {Dense, Sparse, Sparse});
-  //noncov_mp2_fused(Int, C, Phat, X, L);
-  //Tensor<double> X_unf("X", {PAO_HAT, AUX, MO}, {Dense, Sparse, Sparse});
-  //noncov_mp2_unfused(Int, C, Phat, X_unf, L);
-  //Tensor<double> X_nary("X", {AUX, MO, PAO_HAT}, {Dense, Sparse, Sparse});
-  //noncov_mp2_nary(Int, C, Phat, X_nary, L);
-  Tensor<double> X_nofilter("X", {AUX, MO, PAO_HAT}, {Dense, Sparse, Sparse});
+  Tensor<double> X("X", {AUX, MO, PAO_HAT}, {Dense, Sparse, Sparse});
+  filter_fused(Int, C, Phat, X, L);
+  // Tensor<double> X_unf("X", {PAO_HAT, AUX, MO}, {Dense, Sparse, Sparse});
+  // noncov_mp2_unfused(Int, C, Phat, X_unf, L);
+  // Tensor<double> X_nary("X", {AUX, MO, PAO_HAT}, {Dense, Sparse, Sparse});
+  // noncov_mp2_nary(Int, C, Phat, X_nary, L);
+  Tensor<double> X_nofilter("X", {MO, AUX, PAO_HAT}, {Dense, Sparse, Sparse});
   nofilter_fused(Int, C, X_nofilter, Phat);
-  //Tensor<double> X_nofilter_unf("X", {AUX, MO, PAO_HAT},
-  //                              {Dense, Sparse, Sparse});
-  //nofilter_unfused(Int, C, X_nofilter_unf, Phat);
-  //Tensor<double> X_nofilter_nary("X", {AUX, MO, PAO_HAT},
+  // Tensor<double> X_nofilter_unf("X", {AUX, MO, PAO_HAT},
   //                               {Dense, Sparse, Sparse});
-  //nofilter_nary(Int, C, X_nofilter_nary, Phat);
+  // nofilter_unfused(Int, C, X_nofilter_unf, Phat);
+  // Tensor<double> X_nofilter_nary("X", {AUX, MO, PAO_HAT},
+  //                                {Dense, Sparse, Sparse});
+  // nofilter_nary(Int, C, X_nofilter_nary, Phat);
   return 0;
 }
