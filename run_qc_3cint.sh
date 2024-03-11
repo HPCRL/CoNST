4e660efe1c68895fc9abeaf99c94755557f9ddd4
Saurabh <saurabh.raje@utah.edu>
2024-03-11 00:46:20 -0600

    aand more scripts!

---
+++ run_qc_3cint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# first we make the data
+cd taco/example
+./make_3cint_data.sh
+cd ../..
+# now generate code
+python test_tree_3c.py
+# run the code
+cd taco/example
+./run_mp2.sh
+cd ../..
+# process the logs to make graphs
+cd graphs
+python mp2_3c.py
+cd ..
---
+++ taco/example/run_mp2.sh
@@ -0,0 +1,4 @@
+# run small size
+./compile.sh mp2_fusion small &> ../../graphs/mp2_log_small.txt
+./compile.sh mp2_fusion medium &> ../../graphs/mp2_log_medium.txt
+./compile.sh mp2_fusion large &> ../../graphs/mp2_log_large.txt
